6e17568e6614cf0640f9fb4cbbfa39ea523eb0ac
Tongwei Dai <tongweid@gmail.com>
2024-05-05 20:00:36 -0400

    update rebuild

--- auto_note_rebuild.py
+++ auto_note_rebuild.py
@@ -4,22 +4,33 @@ import subprocess
 import sys
 from queue import Queue
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
 from threading import Lock, Thread, Event
 import os
 
-g_shutdown = Event() 
-g_changed_files = set()
+if __name__ == '__main__':
+    g_shutdown = Event() 
+    g_changed_files = set()
+
+    SYM_LINK = ".\\content"
+    if not os.path.exists(SYM_LINK):
+        print(f"Error: symlink {SYM_LINK} does not exist.")
+        sys.exit(1)
+
+    if os.path.islink(SYM_LINK):
+        DIRECTORY_TO_WATCH = os.path.realpath(SYM_LINK)
+    else:
+        DIRECTORY_TO_WATCH = SYM_LINK
+
+    print(f"Watching {DIRECTORY_TO_WATCH} for changes.")
 
 class Watcher:
-    DIRECTORY_TO_WATCH = "C:\\Users\\Administrator\\Dropbox\\NOTES\\obsidian\\Coding"
-
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
         event_handler = Handler()
-        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.schedule(event_handler, DIRECTORY_TO_WATCH, recursive=True)
         self.observer.start()
 
         try:
@@ -32,11 +43,8 @@ class Watcher:
 
 class Handler(FileSystemEventHandler):
     @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return
-        # print(f"Received FS event {event.event_type} - {event.src_path}.")
-        if event.src_path.endswith("workspace.json"):
+    def on_any_event(event : FileSystemEvent):
+        if event.is_directory or event.src_path.endswith("workspace.json"):
             return
         g_changed_files.add(event.src_path)
 
@@ -44,36 +52,35 @@ class UpdateThread(Thread):
     def __init__(self, interval):
         super().__init__()
         self.interval = interval
+        self.wait_event = Event()
 
-    def any_file_blocked(self):
+    def wait_for_files(self):
         to_del = []
         for file_path in g_changed_files:
+            while os.path.isfile(file_path):
+                try:
+                    with open(file_path, 'r'):
+                        break # file is readable, continue
+                except:
+                    self.wait_event.wait(2)
             if not os.path.isfile(file_path):
                 to_del.append(file_path)
-                continue
-            try:
-                with open(file_path, 'r+'):
-                    pass  # Attempt to open the file
-            except Exception as e:
-                return True
+
         for file_path in to_del:
-            g_changed_files.remove(file_path)
-        return False
+            try:
+                g_changed_files.remove(file_path)
+            except:
+                pass
     
     def run(self) -> None:
         while not g_shutdown.is_set():
             if len(g_changed_files) > 0:
                 print("changes detected, rebuilding website...")
 
-                while(self.any_file_blocked()):
-                    time.sleep(5)
+                self.wait_for_files()
                 print(f"the following files have been changed:\n{g_changed_files}")
 
-                p = subprocess.Popen(["powershell.exe", "C:\\Users\\Administrator\\Desktop\\servers\\deploy_notes.ps1"], 
-                                     stdin=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                p.stdin.write('\n')
-                p.stdin.flush()
-                p.stdin.close()
+                p = subprocess.Popen(["npx", "quartz", "build"], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, text=True, shell=True)
                 p.communicate()
                 rc = p.returncode
                 if rc == 0:
