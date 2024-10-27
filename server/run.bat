813442c8fe4d0724113d319aaf71d78c84cba2c6
Tongwei Dai <tongweid@gmail.com>
2024-10-27 16:03:38 -0700

    bug fix

--- server/run.bat
+++ server/run.bat
@@ -1,7 +1,8 @@
 @echo off
 
-:: Change to the directory where pyproject.toml is located
-cd /d "C:\Users\Administrator\Desktop\servers\NotesEggpoker\server"
+pushd %~dp0
 
 :: Run the server using the poetry script defined in pyproject.toml
 poetry run waitress-serve --port=8081 --url-scheme=https main:app
+
+popd
